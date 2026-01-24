2c1ec1408128f4d24194173e36781cacd1554593
Prathmesh Aher <prathameshaher456@gmail.com>
2026-01-24 23:24:02 +0530

    code modifications

--- hms-spring-backend/src/main/java/com/backend/repository/UserRepository.java
+++ hms-spring-backend/src/main/java/com/backend/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package com.backend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.backend.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	boolean existsByEmail(String em);
 
+	Optional<User> findByEmailAndPassword(String email, String password);
+
+	Optional<User> findByEmail(String email);
 }
